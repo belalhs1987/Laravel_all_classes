a15b432d3f25840b4419224df680266ba5961746
belalhs1987 <belalhs@gmail.com>
2018-10-27 18:29:58 +0600

    classes

---
+++ class-51/app/Http/Controllers/practiceController3.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class practiceController3 extends Controller
+{
+     public function methodName($data1,$data2){
+   	return view('welcome3');
+   }
+}
